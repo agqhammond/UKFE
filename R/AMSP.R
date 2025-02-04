67cc841d318f22746c5d487f4e209dba8b39b5d1
Anthony Hammond <anthony.hammond@jbaconsulting.com>
2025-02-04 11:05:17 +0000

    Initial Configuration

---
+++ R/AMSP.R
@@ -0,0 +1,13 @@
+#' National River Flow Archive (NRFA) annual maximum data for sites suitable for pooling
+#'
+#' A data.frame with three columns; Date, Flow, id. NRFA Peak Flow Dataset - Version 13.0.2
+#'
+#'
+#' @format A data frame with 26539 rows and 3 columns
+#' \describe{
+#'   \item{Date}{Date}
+#'   \item{Flow}{Annual maximum peak flow, m3/s}
+#'   \item{id}{Station identification number}
+#' }
+#' @source \url{https://nrfa.ceh.ac.uk/peak-flow-dataset}
+"AMSP"
